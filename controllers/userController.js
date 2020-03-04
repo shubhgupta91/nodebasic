cc4be746b27a4ae60237c1d79b506c60578e53bc
Rahul Kumar <rkumar@VELOCIS.IN>
2020-03-04 12:01:10 +0530

    commit -m 'rename the name of user model and create schema for user regis

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-var User = require('../models/users');
+var User = require('../models/users.model');
 // Handle book create on POST.
 exports.register = function(req, res) {
   res.send('NOT IMPLEMENTED: Book create POST');
---
+++ models/users.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+var userregisterSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: 'This field is required.'
+    },
+    lastname: {
+        type: String,
+        required: 'This field is required.'
+    },
+    email: {
+        type: String,
+        required: 'This field is required.'
+    },
+    mobile: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+});
+
+mongoose.model('Userregister', userregisterSchema);
