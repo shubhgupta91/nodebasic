eb3743fbd9d3943661b0e15db6e36949170270ad
Rahul Kumar <rkumar@VELOCIS.IN>
2020-03-04 15:23:10 +0530

    show user listing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,7 @@
 var userModel = require('../models/users.model');
 mongoose = require('mongoose'), //mongo connection
 
-// Handle book create on POST.
+// Create user in mongodb database
 exports.register = function(req, res) {
   var email = req.body.email;
   var firstname = req.body.fname;
@@ -9,8 +9,6 @@ exports.register = function(req, res) {
   var mobile = req.body.mobile;
   var city = req.body.city;
   var password = req.body.psw;
-
-  //call the create function for our database
   mongoose.model('Users').create({
     firstname : firstname,
     lastname : lastname,
@@ -20,24 +18,39 @@ exports.register = function(req, res) {
     password : password
   }, function (err, user) {
     if (err) {
-      res.send("There was a problem adding the information to the database.");
+      res.send("There was a problem adding user into the database.");
     } else {
-      //User has been created
       console.log('POST creating new user: ' + user);
       res.format({
-          //HTML response will set the location and redirect back to the home page. You could also create a 'success' page if that's your thing
         html: function(){
-          // If it worked, set the header so the address bar doesn't still say /adduser
           res.location("user/listing");
-          // And forward to success page
           res.redirect("/user/listing");
         },
-        //JSON response will show the newly created blob
         json: function(){
           res.json(user);
         }
       });
     }
-  });
-  
+  });  
 };
+
+// Fetch users from mongodb database
+exports.userlisting = function(req, res) {
+  mongoose.model('Users').find({}, function (err, users) {
+    if(err){
+      return console.error(err);
+    } else {
+      res.format({ 
+        html: function(){
+          res.render('user/userlisting', {
+            title: 'Users',
+            "users" : users
+          });
+        },
+        json: function(){
+          res.json(users);
+        }
+      });
+    }
+  });
+}; 
--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,19 @@ var router = express.Router();
 // Require controller modules.
 var user_controller = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// User listing
+router.get('/listing', user_controller.userlisting);
 
+// Login user
 router.get('/login', function(req, res, next) {
   res.render('user/login', { title: 'Login' });
 });
 
-
+// User registration
 router.get('/register', function(req, res, next) {
   res.render('user/register', { title: 'Registration' });
 });
-
 router.post('/register', user_controller.register);
+
 
 module.exports = router;
