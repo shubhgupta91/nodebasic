39639af79d867af0f3651dc3d612ed87e13024fa
Rahul Kumar <rkumar@velocis.in>
2020-03-11 15:08:14 +0530

    a

--- public/js/banner.js
+++ public/js/banner.js
@@ -17,11 +17,13 @@ function showSlides(n) {
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides[i].style.opacity = "0";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.opacity = "1";
   dots[slideIndex-1].className += " active";
 }
 /************** Slideshow banner End ****************/
